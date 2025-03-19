147a0f9713de435648426c00ec14c8c1a6fa8e0a
Thiago Mazzi <thiagodev728@gmail.com>
2025-03-19 16:04:33 -0300

    Arrumando movieCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import Banner from "./components/banner/Banner";
 import MovieCard from "./components/movieCard/MovieCard";
-
+import NavBar from "./components/navbar/NavBar";
+import Banner from "./components/banner/Banner";
 const App = () => {
   return (
-    <div className="container py-4 px-3 mx-auto">
-      <Banner />
-    </div>
-
+    <>
+    </>
   );
 };
 
