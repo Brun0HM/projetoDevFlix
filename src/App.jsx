d5eaa9a83673f664b0b38668bb3cc02c34ed92b4
Brun0 <brunodev41@gmail.com>
2025-03-19 15:07:28 -0300

    implementação de scroll suave

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import Banner from "./components/banner/Banner";
 import MovieCard from "./components/movieCard/MovieCard";
+import NavBar from "./components/navbar/NavBar";
 
 const App = () => {
   return (
-    <div className="container py-4 px-3 mx-auto">
-      <Banner />
+
+    <div className="">
+     
     </div>
 
   );
