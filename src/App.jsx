ce174d0f5433996ddef636de5f89acb257952661
Brun0 <brunodev41@gmail.com>
2025-03-19 09:06:34 -0300

    Inicando projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+
+
+const App = () => {
+  return (
+    <div className="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button className="btn btn-danger">Click me</button>
+    </div>
+  );
+};
+
+export default App;
