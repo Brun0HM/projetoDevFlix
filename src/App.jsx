f9d1adeb48a141afabecf9397c3b04547553ae6b
Brun0 <brunodev41@gmail.com>
2025-03-19 14:32:39 -0300

    Finalização NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import NavBar from "./components/navbar/NavBar";
-
-
-
 const App = () => {
-  return (
-    <div>
-  <NavBar />
-  </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,11 +2,75 @@ import styles from "./NavBar.module.css";
 
 const NavBar = (props) => {
   return (
-    <header className="bg-dark text-white p-3 d-flex">
-<img src="https://placehold.co/180x60" alt="logo" className="ms-5" />
-      <a className="ms-5 text-decoration-none text-light">Inicio</a>
-      <a className="ms-5 text-decoration-none text-light">Categorias</a>
-      <a className="ms-5 text-decoration-none text-light">Séries</a>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-black p-3">
+        <div className="container-fluid">
+          <a className="navbar-brand ms-5" href="#">
+            <img src="https://placehold.co/180x60" alt="logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link text-light"
+                  href="#"
+                  data-bs-toggle="button"
+                  aria-pressed="false"
+                >
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link text-light"
+                  href="#"
+                  data-bs-toggle="button"
+                  aria-pressed="false"
+                >
+                  Categorias
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link text-light"
+                  href="#"
+                  data-bs-toggle="button"
+                  aria-pressed="false"
+                >
+                  Séries
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* Barra de pesquisa fora do menu toggle */}
+          <div className={`d-flex align-items-center ${styles.back}`}>
+            <span className="text-light">
+              <ion-icon name="search-sharp"></ion-icon>
+            </span>
+            <input
+              className={`${styles.searchInput} ms-2 text-light rounded bg-dark border-0 form`}
+              placeholder="Nome do filme"
+              type="text"
+            />
+            <img
+              src="https://placehold.co/50x50"
+              alt="profile"
+              className="rounded-2 ms-3"
+            />
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
