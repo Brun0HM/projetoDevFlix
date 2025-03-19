8f86b9713ee3d4c15aa14ce6ba0d4ef5bd45c311
Rattini <rattinicursods@gmail.com>
2025-03-19 14:23:19 -0300

    correção erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-<<<<<<< HEAD
+import Banner from "./components/banner/Banner";
 import MovieCard from "./components/movieCard/MovieCard";
 
 const App = () => {
   return (
     <div className="container py-4 px-3 mx-auto">
+      <Banner />
     </div>
-=======
-import Banner from "./components/banner/Banner";
 
-const App = () => {
-  return (
-    <Banner />
->>>>>>> rattinimestredouniverso
   );
 };
 
