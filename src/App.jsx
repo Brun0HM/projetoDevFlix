ffba89f703d9198c972429a2830f96a07e62afe9
Thiago Mazzi <thiagodev728@gmail.com>
2025-03-26 08:14:41 -0300

    Adicionando filmes de terror e ação

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,58 @@ import logo from "./assets/logo.png";
 const App = () => {
   const [search, setSearch] = useState("");
   const [movies, setMovies] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [actionMovies, setActionMovies] = useState([]);
+  const [horrorMovies, setHorrorMovies] = useState([]);
   const url = "https://api.themoviedb.org/3/movie/popular?language=pt-BR";
   const accessToken =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNmYxMWFkYWM2NWRmMTgyYTE2OTkwMTFjNDBjZmQxMCIsIm5iZiI6MTc0MjQxMjUxNS41NDU5OTk4LCJzdWIiOiI2N2RiMWFlMzA4NzQ5ZWZlMzRlNzdkZWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Vp784RFA4xs-7fVY5iWZdz7jnrSMHh_0ZPngKa_TNNY";
-
-  // fetch(url, {
-  //   method: "GET",
-  //   headers: {
-  //     Authorization: `Bearer ${accessToken}`,
-  //     Accept: "application/json",
-  //   },
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => setMovies(data.results))
-  //   .catch((error) =>
-  //     console.error("Erro ao buscar os filmes populares:", error)
-  //   );
   useEffect(() => {
-    searchMovie("avengers");
+    searchMovie("batman");
   }, []);
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => setPopular(data.results))
+    .catch((error) =>
+      console.error("Erro ao buscar os filmes populares:", error)
+    );
+
+const url3 =`https://api.themoviedb.org/3/discover/movie?language=pt-BR&with_genres=27`
+fetch(url3, {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+    Accept: "application/json"
+  },
+})
+.then((response) => response.json())
+.then((data) => setHorrorMovies(data.results))
+.catch((error) =>
+  console.error("Erro ao buscar os filmes de terror:", error)
+);
+
+
+
+  const url2 = `https://api.themoviedb.org/3/discover/movie?language=pt-BR&with_genres=28`;
+  fetch(url2, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => setActionMovies(data.results))
+    .catch((error) =>
+      console.error("Erro ao buscar os filmes de ação:", error)
+    );
 
   const searchMovie = async (titulo) => {
     const url2 = `https://api.themoviedb.org/3/search/movie?language=pt-BR&query=${titulo}`;
@@ -46,8 +79,8 @@ const App = () => {
   const handleSearch = (e) => {
     e.key === "Enter" && searchMovie(search);
   };
-  const mostPopularMovie = movies.length > 0 ? movies[0] : null;
-  //   console.log(apiUrl);
+  const mostPopularMovie = popular.length > 0 ? popular[0] : null;
+
   return (
     <>
       <div className="bg-black">
@@ -58,12 +91,32 @@ const App = () => {
         />
         {mostPopularMovie && <Banner movie={mostPopularMovie} />}
         <div>
+          <h3 className="text-light m-3">Lista</h3>
           <div className="d-flex overflow-x-scroll scroller">
             {movies.map((movie, index) => (
               <MovieCard key={index} {...movie} />
             ))}
           </div>
         </div>
+
+        <div>
+          <h3 className="text-light m-3">Ação</h3>
+          <div className="d-flex overflow-x-scroll scroller">
+            {actionMovies.map((movie, index) => (
+              <MovieCard key={index} {...movie} />
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h3 className="text-light m-3">Terror</h3>
+          <div className="d-flex overflow-x-scroll scroller">
+            {horrorMovies.map((movie, index) => (
+              <MovieCard key={index} {...movie} />
+            ))}
+          </div>
+        </div>
+
       </div>
     </>
   );
